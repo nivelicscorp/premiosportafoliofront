7cb2b3cf191cc8db0937973338a380ffa4b27aa3
Nicolas_SuRo <nicolas.suarez@nivelics.co>
2024-04-02 08:46:26 -0500

    feat: added testing page to check dependencies version and functionality

---
+++ pages/test-page/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+import BasicSwiper from '../../components/molecules/BasicSwiper/BasicSwiper';
+
+const TestingPage: NextPage = () => {
+  return (
+    <div>
+      <BasicSwiper />
+    </div>
+  );
+};
+
+export default TestingPage;
