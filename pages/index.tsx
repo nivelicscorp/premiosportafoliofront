28c2fc605c7426f0d478cce8aa9dbce75d24a14a
Nicolas_SuRo <55769562+Niksuro@users.noreply.github.com>
2024-04-09 13:37:43 -0500

    feat: added SSR to Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
 import { BtnFloat } from '@atoms/BtnFloat/BtnFloat'
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 const Home: NextPage = () => {
   return (
     <>
       <BtnFloat />
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {},
+  }
 }
 
 export default Home
