d755c77192def155cea40e659436c9ccfcc4b086
CamilaArteagaHernandez <camila.arteaga@nivelics.co>
2024-05-15 16:47:03 -0500

    form register

--- components/molecules/Introduccionblock/Introduccionblock.tsx
+++ components/molecules/Introduccionblock/Introduccionblock.tsx
@@ -1,18 +1,28 @@
 import Head from '@atoms/Head/Head'
-import React from 'react'
+import React, { ReactNode } from 'react'
 import Image from 'next/image'
 import styles from '@styles/scss/molecules/introduccionblock.module.scss'
 
-const Introduccionblock = () => {
+interface introBlock {
+  textTitle: string
+  children?: ReactNode
+}
+
+const Introduccionblock = ({ textTitle, children }: introBlock) => {
   return (
     <div className={styles?.introduccionblock}>
-        <Head text='Regístrate en Premios Portafolio 2024' />
-        <div className={styles?.introduccionblock__info}>
-            <div className={styles?.introduccionblock__info__img}>
-              <Image src='/img/forms/prize-lineblack.svg' alt="" width={20} height={46}/>
-            </div>
-            <p> Si quiere realizar el proceso de <strong> inscripción como empresa y persona, </strong> deberá hacer el registro con <mark> usuarios diferentes </mark>  para cada postulación. </p>
+      <Head text={textTitle} />
+      <div className={styles?.introduccionblock__info}>
+        <div className={styles?.introduccionblock__info__img}>
+          <Image
+            src='/img/forms/prize-lineblack.svg'
+            alt=''
+            width={20}
+            height={46}
+          />
         </div>
+        <div>{children}</div>
+      </div>
     </div>
   )
 }
--- pages/registro/index.tsx
+++ pages/registro/index.tsx
@@ -1,9 +1,19 @@
 import { GetServerSideProps } from 'next'
 import RegisterForm from '@molecules/RegisterForm/RegisterForm'
+import Introduccionblock from '@molecules/Introduccionblock/Introduccionblock'
+import MainBanner from '@molecules/MainBanner/MainBanner'
 
 const UserPage = () => {
   return (
     <>
+      <Introduccionblock textTitle='Regístrate en Premios Portafolio 2024'>
+        <p>
+          {' '}
+          Si quiere realizar el proceso de{' '}
+          <strong> inscripción como empresa y persona, </strong> deberá hacer el
+          registro con <mark> usuarios diferentes </mark> para cada postulación.{' '}
+        </p>
+      </Introduccionblock>
       <RegisterForm />
     </>
   )
