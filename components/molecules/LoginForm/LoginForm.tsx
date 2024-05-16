20b8d7ba83a5b81b39752e3a6be19bb46df3ac2f
CamilaArteagaHernandez <camila.arteaga@nivelics.co>
2024-05-16 12:31:52 -0500

    login mq

--- components/molecules/LoginForm/LoginForm.tsx
+++ components/molecules/LoginForm/LoginForm.tsx
@@ -62,56 +62,77 @@ const LoginForm = () => {
       })
   }
   return (
-    <div
-      style={{
-        margin: '50px auto',
-        textAlign: 'center',
-        width: '50%',
-      }}
-    >
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '1rem',
-        }}
-      >
-        <Input
-          type='text'
-          placeholder='Correo...'
-          label='Correo'
-          errorMessage={errors?.email?.message?.toString()}
-          {...register('email', {
-            required: 'El correo es requerido.',
-            // pattern: {
-            //   value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-            //   message: 'El correo no es válido.',
-            // },
-          })}
-        />
-        <Input
-          type='password'
-          placeholder='Contraseña...'
-          label='Contraseña'
-          errorMessage={errors?.password?.message?.toString()}
-          {...register('password', { required: 'La contraseña es requerida.' })}
-        />
-        <Button
-          title='Iniciar sesión'
-          variant='secondary'
-          disabled={sendingData}
-        />
+
+    <section className={'form-content'}>
+      <form onSubmit={handleSubmit(onSubmit)} className='form' >
+        <fieldset className={'form__fieldset'}>
+          <div className='form__top'>
+            <h1 className='form__top__title'> Bienvenido de nuevo </h1>
+            <div className='form__top__img'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="66" height="66" viewBox="0 0 66 66">
+                <defs>
+                  <clipPath id="clip-path">
+                    <rect id="Rectángulo_64692" data-name="Rectángulo 64692" width="24" height="24" transform="translate(-0.377 0)" fill="#fff" stroke="#707070" stroke-width="1" />
+                  </clipPath>
+                </defs>
+                <g id="Grupo_112056" data-name="Grupo 112056" transform="translate(0.377 0)">
+                  <g id="Icon_-_person_-_user" data-name="Icon - person - user" transform="translate(14.148 14)">
+                    <g id="Grupo_108120" data-name="Grupo 108120">
+                      <path id="Trazado_391984" data-name="Trazado 391984" d="M18.923,0A18.923,18.923,0,1,1,0,18.923,18.923,18.923,0,0,1,18.923,0Z" fill="#31abaa" />
+                      <g id="Enmascarar_grupo_62127" data-name="Enmascarar grupo 62127" transform="translate(6.852 7)" clip-path="url(#clip-path)">
+                        <path id="Icon_awesome-user-alt" data-name="Icon awesome-user-alt" d="M9.694,10.905A5.453,5.453,0,1,0,4.241,5.453,5.454,5.454,0,0,0,9.694,10.905Zm4.847,1.212H12.454a6.592,6.592,0,0,1-5.521,0H4.847A4.846,4.846,0,0,0,0,16.964v.606a1.818,1.818,0,0,0,1.818,1.818H17.57a1.818,1.818,0,0,0,1.818-1.818v-.606A4.846,4.846,0,0,0,14.541,12.117Z" transform="translate(2.377 2.229)" fill="#fff" />
+                      </g>
+                    </g>
+                  </g>
+                  <g id="Elipse_1905" data-name="Elipse 1905" transform="translate(-0.377 0)" fill="none" stroke="#31abaa" stroke-width="2">
+                    <circle cx="33" cy="33" r="33" stroke="none" />
+                    <circle cx="33" cy="33" r="32" fill="none" />
+                  </g>
+                </g>
+              </svg>
+            </div>
+          </div>
+          <Input
+            type='text'
+            placeholder='Correo...'
+            label='Correo'
+            errorMessage={errors?.email?.message?.toString()}
+            {...register('email', {
+              required: 'El correo es requerido.',
+              // pattern: {
+              //   value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+              //   message: 'El correo no es válido.',
+              // },
+            })}
+          />
+          <Input
+            type='password'
+            placeholder='Contraseña...'
+            label='Contraseña'
+            errorMessage={errors?.password?.message?.toString()}
+            {...register('password', { required: 'La contraseña es requerida.' })}
+          />
+          <div className='form__btn'>
+            <Button
+              title='Iniciar sesión'
+              variant='secondary'
+              disabled={sendingData}
+            />
+          </div>
+        </fieldset>
+
       </form>
-      <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-        <Link href='/registro' passHref scroll={false}>
-          <a>¿Aún no estas registrado? ¡Regístrate ahora!</a>
-        </Link>
+      <div className='form__links'>
         <Link href='/recuperar-contrasena' passHref scroll={false}>
           <a>¿Olvidaste tu contraseña?</a>
         </Link>
       </div>
-    </div>
+      <div className='form__links'>
+        <Link href='/registro' passHref scroll={false}>
+          <a>¿Aún no estas registrado? <span> ¡Regístrate ahora! </span> </a>
+        </Link>
+      </div>
+    </section>
   )
 }
 
